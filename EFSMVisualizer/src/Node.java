8e234ab5ef486b1f847e762dec840ba0a9eb858d
Kyeong Wook Ma <shwarzes89@gmail.com>
2014-12-12 07:50:39 +0900

    init commit

---
+++ EFSMVisualizer/src/Node.java
@@ -0,0 +1,16 @@
+
+public class Node {
+
+	private int stateId;
+
+	public int getStateId() {
+		return stateId;
+	}
+
+	public void setStateId(int stateId) {
+		this.stateId = stateId;
+	}
+	
+	
+	
+}
