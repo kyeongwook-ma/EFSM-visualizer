15284b29737ddded804adc5e6163c2b83e3015a2
shwarzes89 <shwarzes89@gmail.com>
2015-02-05 15:20:21 +0900

    implement image cacher

--- EFSMVisualizer/src/model/ImageCacher.java
+++ EFSMVisualizer/src/model/ImageCacher.java
@@ -1,8 +1,42 @@
 package model;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
+
+import javax.imageio.ImageIO;
 
 public class ImageCacher {
-	private HashMap<String, BufferedImage> imageCache;
+	
+	private static HashMap<String, BufferedImage> imageCache
+		= new HashMap<String, BufferedImage>();
+	
+	public static void load() {
+		
+		if(imageCache == null) return;
+		
+		for(String filePath : getFileList()) {
+			try {
+				imageCache.put(extractFileName(filePath), ImageIO.read(new File(filePath)));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private static String[] getFileList() {
+		return new File("./img").list();
+	}
+	
+	private static String extractFileName(String filePath) {
+		int lastDelimIdx = filePath.lastIndexOf("\\");
+		return filePath.substring(lastDelimIdx);
+	}
+	
+	public static BufferedImage getImage(String fileName) throws Exception {
+		if(imageCache == null) throw new Exception();
+		return imageCache.get(fileName);
+	}
 }
