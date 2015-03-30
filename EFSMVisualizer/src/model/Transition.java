a732c821379afe11abb3a3b682e8f250ee7c1ff9
Kyeong Wook Ma <shwarzes89@gmail.com>
2015-03-30 12:00:53 +0900

    ㅇ

--- EFSMVisualizer/src/model/Transition.java
+++ EFSMVisualizer/src/model/Transition.java
@@ -9,21 +9,22 @@ import java.util.Iterator;
  * �����(src)�� ������(dst)�� ������ �ִ�.
  */
 public class Transition implements XMLGenerator {
-	
+
 	private State src, dst;
 	private double timestamp;
-	private double x, y;
 	private String event;
+	private Point touchPoint;
 	private HashSet<String> targets;
-	
+
+
 	public void setSrc(State src) {
 		this.src = src;
 	}
-	
+
 	public void setDst(State dst) {
 		this.dst = dst;
 	}
-	
+
 	public State getSrc() {
 		return src;
 	}
@@ -40,20 +41,17 @@ public class Transition implements XMLGenerator {
 		this.timestamp = timestamp;
 	}
 
-	public double getX() {
-		return x;
+
+	public Point getTouchPoint() {
+		return touchPoint;
 	}
 
-	public void setX(double x) {
-		this.x = x;
+	public void setTouchPoint(Point touchPoint) {
+		this.touchPoint = touchPoint;
 	}
 
-	public double getY() {
-		return y;
-	}
-
-	public void setY(double y) {
-		this.y = y;
+	public void setTargets(HashSet<String> targets) {
+		this.targets = targets;
 	}
 
 	public String getEvent() {
@@ -63,19 +61,19 @@ public class Transition implements XMLGenerator {
 	public void setEvent(String event) {
 		this.event = event;
 	}
-	
+
 	public void addTarget(HashSet<String> targets) {
 		this.targets.addAll(targets);
 	}
-	
+
 	public Transition(State src, State dst) {
 		this.src = src;
 		this.dst = dst;
 		this.targets = new HashSet<String>();
 	}
-	
-	
-	
+
+
+
 	/**
 	 * @author se
 	 * Trnasition�� builder Ŭ����
@@ -88,104 +86,102 @@ public class Transition implements XMLGenerator {
 	 * 
 	 */
 	public static class TransitionBuilder {
-		
+
 		private State src, dst;
 		private double timestamp;
-		private double x, y;
+		private Point touchPoint;
 		private String event;
 		private HashSet<String> targets;
-		
+
 		public TransitionBuilder(State src, State dst) {
-			
+
 			this.src = src;
 			this.dst = dst;
-					
+
 			targets = new HashSet<String>();
 		}
-		
-		public TransitionBuilder x(double x) {
-			this.x = x;
+
+		public TransitionBuilder point(Point p) {
+			this.touchPoint = p;
 			return this;
 		}
-		
-		public TransitionBuilder y(double y) {
-			this.y = y;
-			return this;
-		}
-		
+
 		public TransitionBuilder event(String event) {
 			this.event = event;
 			return this;
 		}
-		
+
 		public TransitionBuilder timestamp(double timestamp) {
 			this.timestamp = timestamp;
 			return this;
 		}
-		
+
 		public TransitionBuilder target(String target) {
 			this.targets.add(target);
 			return this;
 		}
-		
+
 		/**
 		 * @return �� ��ü�� ����� ������ ��� transition instance
 		 */
 		public Transition createTransition() {
 			Transition t = new Transition(this.src, this.dst);
 			t.setEvent(this.event);
-			t.setX(this.x);
-			t.setY(this.y);
+			t.setTouchPoint(this.touchPoint);
 			t.addTarget(targets);
 			t.setTimestamp(this.timestamp);
 			return t;
 		}
 	}
-		
+
 	public Iterator<String> getTargets() {
 		return targets.iterator();
 	}
-	
+
 	public void expend(Transition transition) {
-		
-		double transitionX = transition.getX();
-		double transitionY = transition.getY();
-		
+
+		double transitionX = transition.getTouchPoint().getX();
+		double transitionY = transition.getTouchPoint().getY();
+
 		/* �� ���� ������ ���� �Ҵ��� */
-		x = x - transitionX > 0 ? transitionX : x; 
-		y = y - transitionX > 0 ? transitionY : y; 
-		
+		this.touchPoint.setX(this.touchPoint.getX() - transitionX > 0 ? 
+				transitionX : 
+				this.getTouchPoint().getX()); 
+
+		this.touchPoint.setY(this.touchPoint.getY() - transitionX > 0 ? 
+				transitionY : 
+				this.getTouchPoint().getY()); 
+
 		/* target �߰� */
 		Iterator<String> transitionTarget = transition.getTargets();
-		while(transitionTarget.hasNext()) {
-			String target = transitionTarget.next();
-			this.targets.add(target);
+			while(transitionTarget.hasNext()) {
+				String target = transitionTarget.next();
+				this.targets.add(target);
 		}
-	
+
 	}
-	
 
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		sb.append( "\n" + src.toString() + " -> " + dst.toString() + "\n");
-		sb.append("x : " + x + " ,");
-		sb.append("y : " + y + " ,");
+		sb.append("x : " + this.getTouchPoint().getX() + " ,");
+		sb.append("y : " + this.getTouchPoint().getY() + " ,");
 		sb.append("event : " + event + " ,");
-		
+
 		Iterator<String> transitionTarget = getTargets();
 		StringBuilder targetSB = new StringBuilder();
 		while(transitionTarget.hasNext()) {
 			String target = transitionTarget.next();
 			targetSB.append(" " + target);
 		}
-		
+
 		sb.append("target : " + targetSB.toString() + " ,");
 		sb.append("timestamp : " + String.valueOf(timestamp));
-	
+
 		return sb.toString();
 	}
-	
+
 	/* (non-Javadoc)
 	 * Transition�� event�� ���� ��� ���� transition���� ��� 
 	 */
@@ -194,7 +190,7 @@ public class Transition implements XMLGenerator {
 		Transition t = (Transition) obj;
 		return t.src.equals(src) && t.dst.equals(dst);
 	}
-	
+
 	@Override
 	public int hashCode() {
 		return (String.valueOf(src.getStateId()) + 
